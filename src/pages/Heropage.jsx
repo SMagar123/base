a9c2e572dc4d6113ec60c11bb9c88c4c45bd6559
SMagar123 <surajpulamim123@gmail.com>
2023-06-07 00:05:08 +0545

    updates on homepage is made

--- src/pages/Heropage.jsx
+++ src/pages/Heropage.jsx
@@ -1,10 +1,35 @@
-import React from "react";
 import { Navbar } from "../layouts/Navbar";
+import homepageImage from "../assets/images/homepageImage.png";
+import { Button } from "../components";
 export const Heropage = () => {
   return (
     <div className="heropage">
       <div className="backDrop"></div>
       <Navbar />
+      <div className="heropage_desc">
+        <div className="details">
+          <div className="slogan">
+            <h2>We specialize in UI/UX, Web Development, Digital Marketing.</h2>
+          </div>
+          <div className="desc">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              fringilla magna mauris. Nulla fermentum viverra sem eu rhoncus
+              consequat varius nisi quis, posuere magna.
+            </p>
+          </div>
+          <div className="contact">
+            <Button label="Get Started Now" className="contact" />
+            <div className="call">
+              <p>Call us (0123) 456 – 789</p>
+              <p>For any question or concern</p>
+            </div>
+          </div>
+        </div>
+        <div className="image">
+          <img src={homepageImage} alt="homepage" />
+        </div>
+      </div>
     </div>
   );
 };
